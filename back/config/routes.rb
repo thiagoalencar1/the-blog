c43c2485b05c3917416c3c5e01c48efe4aee82e9
Thiago Gondim <talencar@gmail.com>
2022-09-23 21:20:49 -0300

    build: config graphql

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/graphql", to: "graphql#execute"
 end
---
+++ back/spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title { "MyString" }
+    abstract { "MyString" }
+    tags { 1 }
+    content { "MyString" }
+  end
+end
